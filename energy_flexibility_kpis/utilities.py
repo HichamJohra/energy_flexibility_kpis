502fa0039fd80d58e563d7f1173da8b89bf1387f
Kingsley Nweye <etonwana@yahoo.com>
2023-02-14 23:25:04 -0600

    added timestamp preprocessing function

--- energy_flexibility_kpis/utilities.py
+++ energy_flexibility_kpis/utilities.py
@@ -1,7 +1,11 @@
+# import native python modules
 import datetime
 from typing import Union
 
-class Parser:
+# import installed python modules
+import pandas as pd
+
+class Preprocess:
     @staticmethod
     def parse_time(time: Union[str, datetime.time]) -> datetime.time:
         r"""Return datetime.time instance if str instance.
@@ -17,7 +21,7 @@ class Parser:
             datetime.time instance.
         """
 
-        return time if isinstance(time, datetime.time) else Parser.string_to_time(time)
+        return time if isinstance(time, datetime.time) else Preprocess.string_to_time(time)
 
     @staticmethod
     def string_to_time(string: str) -> datetime.time:
@@ -37,3 +41,17 @@ class Parser:
         time = datetime.time(*[int(v) for v in string.split(':')])
 
         return time
+
+    @staticmethod
+    def set_timestamp_fields(df: pd.DataFrame) -> pd.DataFrame:
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+        df['date'] = df['timestamp'].dt.normalize()
+        df['year'] = df['timestamp'].dt.year
+        df['month'] = df['timestamp'].dt.month
+        df['day_of_month'] = df['timestamp'].dt.day
+        df['day_of_week'] = df['timestamp'].dt.day_of_week
+        df['hour'] = df['timestamp'].dt.hour
+        df['day_of_year'] = df['timestamp'].dt.day_of_year
+
+        return df
+    
