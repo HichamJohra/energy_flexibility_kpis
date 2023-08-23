c931ca78ddeb2b2c8462f49d55aa8fe8770b5ad9
Kingsley Nweye <etonwana@yahoo.com>
2023-08-23 17:38:22 -0500

    updated kpi implementations

--- energy_flexibility_kpis/kpi/energy_flexibility/demand_response_energy_efficiency.py
+++ energy_flexibility_kpis/kpi/energy_flexibility/demand_response_energy_efficiency.py
@@ -1,49 +1,98 @@
 import datetime
 from typing import List, Union
+from scipy import integrate
 from energy_flexibility_kpis.kpi.base import KPI
+from energy_flexibility_kpis.enumerations import BaseUnit, Complexity, DOEFlexibilityCategory, KPICategory, PerformanceAspect, Relevance 
+from energy_flexibility_kpis.enumerations import Stakeholder, TemporalEvaluationWindow,TemporalResolution, SpatialResolution
+from energy_flexibility_kpis.unit import Unit
 
 class EnergySavingsOfDemandResponse(KPI):
     """Difference between reference energy usage without demand response and and the energy 
     usage with demand response over the next 24h."""
 
+    NAME = 'energy savings of demand response'
+    DEFINITION = __doc__
+    UNIT = Unit(numerator=[BaseUnit.KWH])
+    CATEGORY = KPICategory.EF_DEMAND_RESPONSE_ENERGY_EFFICIENCY
+    RELEVANCE = Relevance.MEDIUM
+    STAKEHOLDERS = [Stakeholder.BUILDING_OWNER, Stakeholder.BUILDING_OPERATOR]
+    COMPLEXITY = Complexity.LOW
+    NEED_BASELINE = True
+    TEMPORAL_EVALUATION_WINDOW = TemporalEvaluationWindow.WHOLE_DAY
+    TEMPORAL_RESOLUTION = TemporalResolution.HOURLY
+    SPATIAL_RESOLUTION = SpatialResolution.UNSPECIFIED
+    DOE_FLEXIBILITY_CATEGORY = [DOEFlexibilityCategory.EFFICIENCY]
+    PERFORMANCE_ASPECT = [PerformanceAspect.ENERGY]
+
     def __init__(self):
         super().__init__()
 
     @classmethod
     def calculate(
         cls,
-        timestamps: Union[List[int], List[datetime.datetime], List[str]] = None,
+        baseline_electric_power_profile: List[float],
+        flexible_electric_power_profile: List[float],
+        timestamps: Union[List[int], List[datetime.datetime], List[str]],
         evaluation_start_timestamp: Union[int, datetime.datetime, str] = None,
         evaluation_end_timestamp: Union[int, datetime.datetime, str] = None,
     ) -> float:
         _, vs = super().calculate(
+            baseline_electric_power_profile=baseline_electric_power_profile,
+            flexible_electric_power_profile=flexible_electric_power_profile,
             timestamps=timestamps,
             evaluation_start_timestamp=evaluation_start_timestamp,
             evaluation_end_timestamp=evaluation_end_timestamp,
         )
         
-        raise NotImplementedError
-    
+        profile = vs.flexible_electric_power_profile.value[vs.evaluation_mask] - vs.baseline_electric_power_profile.value[vs.evaluation_mask]
+        dx = vs.get_temporal_resolution(BaseUnit.HOUR, value=vs.timestamps.value[vs.evaluation_mask])
+        value = integrate.simpson(profile, dx=dx)
+
+        return value
+
 class EnergyConsumptionRatio(KPI):
     """Change in the total energy consumption when implementing an energy flexibility control strategy."""
 
+    NAME = 'energy consumption ratio'
+    DEFINITION = __doc__
+    UNIT = Unit(numerator=[BaseUnit.DIMENSIONLESS])
+    CATEGORY = KPICategory.EF_DEMAND_RESPONSE_ENERGY_EFFICIENCY
+    RELEVANCE = Relevance.MEDIUM
+    STAKEHOLDERS = [Stakeholder.BUILDING_OPERATOR]
+    COMPLEXITY = Complexity.LOW
+    NEED_BASELINE = True
+    TEMPORAL_EVALUATION_WINDOW = TemporalEvaluationWindow.SINGLE_EVENT
+    TEMPORAL_RESOLUTION = TemporalResolution.HOURLY
+    SPATIAL_RESOLUTION = SpatialResolution.UNSPECIFIED
+    DOE_FLEXIBILITY_CATEGORY = [DOEFlexibilityCategory.EFFICIENCY, DOEFlexibilityCategory.LOAD_SHEDDING]
+    PERFORMANCE_ASPECT = [PerformanceAspect.ENERGY]
+
     def __init__(self):
         super().__init__()
 
     @classmethod
     def calculate(
         cls,
-        timestamps: Union[List[int], List[datetime.datetime], List[str]] = None,
+        baseline_electric_power_profile: List[float],
+        flexible_electric_power_profile: List[float],
+        timestamps: Union[List[int], List[datetime.datetime], List[str]],
         evaluation_start_timestamp: Union[int, datetime.datetime, str] = None,
         evaluation_end_timestamp: Union[int, datetime.datetime, str] = None,
     ) -> float:
         _, vs = super().calculate(
+            baseline_electric_power_profile=baseline_electric_power_profile,
+            flexible_electric_power_profile=flexible_electric_power_profile,
             timestamps=timestamps,
             evaluation_start_timestamp=evaluation_start_timestamp,
             evaluation_end_timestamp=evaluation_end_timestamp,
         )
         
-        raise NotImplementedError
+        baseline_profile = vs.baseline_electric_power_profile.value[vs.evaluation_mask]
+        flexible_profile = vs.flexible_electric_power_profile.value[vs.evaluation_mask]
+        dx = vs.get_temporal_resolution(BaseUnit.HOUR, value=vs.timestamps.value[vs.evaluation_mask])
+        value = integrate.simpson(flexible_profile, dx=dx)/integrate.simpson(baseline_profile, dx=dx)
+
+        return value
     
 class DemandRecoveryRatio(KPI):
     """Ratio between the observed electric energy use by the flexible electric heating systems 
@@ -52,27 +101,64 @@ class DemandRecoveryRatio(KPI):
     electricity generation system, an optimisation towards minimum electrical energy use is 
     performed at the demand side. The DRR will therefore always be greater than or equal to 1."""
 
+    NAME = 'demand recovery ratio'
+    DEFINITION = __doc__
+    UNIT = Unit(numerator=[BaseUnit.DIMENSIONLESS])
+    CATEGORY = KPICategory.EF_DEMAND_RESPONSE_ENERGY_EFFICIENCY
+    RELEVANCE = Relevance.MEDIUM
+    STAKEHOLDERS = [Stakeholder.BUILDING_OPERATOR]
+    COMPLEXITY = Complexity.LOW
+    NEED_BASELINE = True
+    TEMPORAL_EVALUATION_WINDOW = TemporalEvaluationWindow.SINGLE_EVENT
+    TEMPORAL_RESOLUTION = TemporalResolution.UNSPECIFIED
+    SPATIAL_RESOLUTION = SpatialResolution.SINGLE_BUILDING
+    DOE_FLEXIBILITY_CATEGORY = [DOEFlexibilityCategory.EFFICIENCY, DOEFlexibilityCategory.LOAD_SHIFTING, DOEFlexibilityCategory.GENERATION]
+    PERFORMANCE_ASPECT = [PerformanceAspect.ENERGY]
+
     def __init__(self):
         super().__init__()
 
     @classmethod
     def calculate(
         cls,
+        baseline_electricity_consumption_profile: List[float],
+        flexible_electricity_consumption_profile: List[float],
         timestamps: Union[List[int], List[datetime.datetime], List[str]] = None,
         evaluation_start_timestamp: Union[int, datetime.datetime, str] = None,
         evaluation_end_timestamp: Union[int, datetime.datetime, str] = None,
-    ) -> float:
+    ) -> List[float]:
         _, vs = super().calculate(
+            baseline_electricity_consumption_profile=baseline_electricity_consumption_profile,
+            flexible_electricity_consumption_profile=flexible_electricity_consumption_profile,
             timestamps=timestamps,
             evaluation_start_timestamp=evaluation_start_timestamp,
             evaluation_end_timestamp=evaluation_end_timestamp,
         )
         
-        raise NotImplementedError
+        flexible_profile = vs.flexible_electricity_consumption_profile.value[vs.evaluation_mask]
+        baseline_profile = vs.baseline_electricity_consumption_profile.value[vs.evaluation_mask]
+        minimum_energy = min(flexible_profile.min(), baseline_profile.min())
+        value = flexible_profile/minimum_energy
+
+        return value
     
 class ConsistencyWithEnergySavings(KPI):
     """This index compares the energy reduction in the optimized and most energy efficient scenario, respectively."""
 
+    NAME = 'consistency with energy savings'
+    DEFINITION = __doc__
+    UNIT = Unit(numerator=[BaseUnit.DIMENSIONLESS])
+    CATEGORY = KPICategory.EF_DEMAND_RESPONSE_ENERGY_EFFICIENCY
+    RELEVANCE = Relevance.LOW
+    STAKEHOLDERS = [Stakeholder.BUILDING_OWNER, Stakeholder.BUILDING_OPERATOR]
+    COMPLEXITY = Complexity.LOW
+    NEED_BASELINE = True
+    TEMPORAL_EVALUATION_WINDOW = TemporalEvaluationWindow.SINGLE_EVENT
+    TEMPORAL_RESOLUTION = TemporalResolution.UNSPECIFIED
+    SPATIAL_RESOLUTION = SpatialResolution.UNSPECIFIED
+    DOE_FLEXIBILITY_CATEGORY = [DOEFlexibilityCategory.EFFICIENCY]
+    PERFORMANCE_ASPECT = [PerformanceAspect.ENERGY]
+
     def __init__(self):
         super().__init__()
 
@@ -89,4 +175,4 @@ class ConsistencyWithEnergySavings(KPI):
             evaluation_end_timestamp=evaluation_end_timestamp,
         )
         
-        raise NotImplementedError
+        raise NotImplementedError('Variable are unclear')
